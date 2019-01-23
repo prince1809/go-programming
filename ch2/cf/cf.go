84148c598a342ca7e4631981638095e41161ffe7
Prince Kumar <prince18pat@gmail.com>
2019-01-23 18:29:06 +0900

    first & second completeed

---
+++ ch2/cf/cf.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"github.com/prince1809/go-programming/ch2/tempconv"
+	"os"
+	"strconv"
+)
+
+func main() {
+	for _,arg := range os.Args[1:] {
+		t,err := strconv.ParseFloat(arg, 64)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "cf: %v\n", err)
+		}
+		f := tempconv.Fahrenheit(t)
+		c := tempconv.Celsius(t)
+		fmt.Printf("%s = %s, %s = %s\n", f, tempconv.FToC(f), c, tempconv.CToF(c))
+	}
+}
